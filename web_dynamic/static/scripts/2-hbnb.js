7b2b1ab29e2b61873efd6e92d6d2f56c6f01eb16
Andrew <drew29799@gmail.com>
2024-01-13 17:16:16 +0300

    Fixed issues with semistandard compliance

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   const amenities = {};
   $('input[type="checkbox"]').change(function () {
     if (this.checked) {
@@ -7,21 +6,21 @@ $(document).ready(function() {
     } else {
       delete amenities[$(this).data('id')];
     }
-    let chosenAmenities = Object.values(amenities);
+    const chosenAmenities = Object.values(amenities);
     if (chosenAmenities.length > 0) {
-      let selected = Object.values(amenities).join(', ')
-      let selected_truncated = selected.length > 50 ? selected.substring(0, 50) + '...' : selected;
-      $('div.amenities > h4').text(selected_truncated);
+      const selected = Object.values(amenities).join(', ');
+      const selectedTruncated = selected.length > 50 ? selected.substring(0, 50) + '...' : selected;
+      $('div.amenities > h4').text(selectedTruncated);
     } else {
       $('div.amenities > h4').html('&nbsp;');
     }
   });
 
-  $.get( "http://0.0.0.0:5001/api/v1/status/", function( data ) {
-    if (data?.status === 'OK'){
-      $('#api_status').addClass('available')
-    }else {
-      $('#api_status').removeClass('available')
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data?.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
     }
-  })
+  });
 });
